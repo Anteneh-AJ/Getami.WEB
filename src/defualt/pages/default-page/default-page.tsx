c2ea96eabf14f330dee8a6d503f3bfd3d8d164eb
Anteneh <anteneh845@gmail.com>
2019-10-31 23:07:24 +0300

    Added header component

--- src/defualt/pages/default-page/default-page.tsx
+++ src/defualt/pages/default-page/default-page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { action, ACCOUNT } from '../../../account/state/actions/account-action';
-
+import { Header } from "../../../shared/components/header/header";
 export function DefaultPage() {
     const dispatch = useDispatch();
     return (
         <div>
+            <Header />
             <h1>
                 Please login if you have an account. Or Register <button onClick={() => dispatch(action(ACCOUNT.LOGIN))}> click here </button>
             </h1>
